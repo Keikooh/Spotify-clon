bc5fc7870f43f9c9dc647692248d6480f8a899b5
Fernanda Gonzalez <Fernanda.Gonzalez@axolotlsolutions.com>
2025-08-10 08:39:31 -0600

    refactor: create reusable base SearchInput component

--- src/components/partials/MainNavbar.tsx
+++ src/components/partials/MainNavbar.tsx
@@ -1,11 +1,12 @@
 import { MdNavigateNext } from "react-icons/md";
 import { GrFormPrevious } from "react-icons/gr";
 import { AiOutlineHome } from "react-icons/ai";
-import SearchBar from "../SearchBar/SearchBar";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "@components/buttons/Button";
 import type { ButtonProps } from "@shared/types/buttonTypes";
 import { buttonVariants } from "@shared/styles/buttonStyles";
+import SearchInput from "@components/inputs/SearchInput";
+import type { SearchInputProps } from "@shared/types/inputTypes";
 
 type props = {
   image: string;
@@ -39,19 +40,29 @@ const MainNavbar: React.FC<props> = ({ image, name }) => {
     isEnabled: true,
   };
 
+  const searchInput: SearchInputProps = {
+    placeholder: "What do you want to play?",
+    inputStyle: "",
+    redirectTo: "/home/search/",
+  };
+
   return (
     <nav className="h-16 flex bg-black w-full  p-5 justify-between items-center">
+      {/* Navegation buttons */}
       <div className="flex gap-x-2 w-1/3">
         <Button {...toBackButton} />
         <Button {...toForwardButton} />
       </div>
+
+      {/* Search input */}
       <div className="flex gap-2 w-1/3 justify-center">
         <Link to={"/home"} className={buttonVariants.filledGray} title="Home">
           <AiOutlineHome className="text-md" />
         </Link>
-        <SearchBar />
+        <SearchInput {...searchInput} />
       </div>
 
+      {/* User info */}
       <div className="flex gap-2 items-center justify-end w-1/3">
         <span className="font-bold">{name}</span>
         <img className="rounded-full w-8 h-8 " src={image} />
