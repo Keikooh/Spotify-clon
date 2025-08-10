067b7e49152f82bb5efa4a7d45f4a18765e0c1b3
Fernanda Gonzalez <Fernanda.Gonzalez@axolotlsolutions.com>
2025-08-10 10:58:31 -0600

    refactor: restructure routing using createBrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
+
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import { Home, Callback, Login } from "./pages/index";
-import { AppLayout, SearchResultsLayout} from "./layouts/index"
-import {
-  ArtistsResults,
-  AllResults,
-  PlaylistsResults,
-  PodcastsResults,
-  TracksResults,
-  AlbumsResults,
-} from "./pages/search/index";
-import { AlbumDetail, ArtistDetail, PlaylistDetail} from "./pages/details/index"
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router"
+
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/callback" element={<Callback />} />
-      <Route path="/home" element={<AppLayout />}>
-        <Route index element={<Home />} />
-        <Route path="search/:query" element={<SearchResultsLayout />}>
-          <Route index element={<AllResults />} />
-          <Route path="tracks" element={<TracksResults />} />
-          <Route path="albums" element={<AlbumsResults />} />
-          <Route path="artists" element={<ArtistsResults />} />
-          <Route path="playlists" element={<PlaylistsResults />} />
-          <Route path="shows" element={<PodcastsResults />} />
-        </Route>
-        <Route path="playlist/:id" element={<PlaylistDetail/>} />
-        <Route path="album/:id" element={<AlbumDetail />} />
-        <Route path="artist/:id" element={<ArtistDetail/>} />
-      </Route>
-    </Routes>
-  );
+  return <RouterProvider router={router}/>
 }
 
 export default App;
